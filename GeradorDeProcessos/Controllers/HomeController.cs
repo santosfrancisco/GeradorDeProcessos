271593389a5c45df944da5949e0175cf47a42515
N1lks <daniel.anapro@gmail.com>
2016-08-07 14:14:21 -0300

    Porra
    Agora essa porra vai

--- GeradorDeProcessos/Controllers/HomeController.cs
+++ GeradorDeProcessos/Controllers/HomeController.cs
@@ -2,19 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+
 using System.Web.Mvc;
+using GeradorDeProcessos.Repositorios;
 
 namespace GeradorDeProcessos.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        // GET: Home
-        public ActionResult Index()
+        // GET: Login
+        public ActionResult Login()
         {
+            ViewBag.Title = "Seja Bem Vindo(a)";
             return View();
         }
 
-		public ActionResult Configuracoes()
+        [HttpGet]
+        public JsonResult AutenticarLogin(string Login, string Senha)
+        {
+            if (RepositorioUsuarios.AutenticarUsuario(Login, Senha))
+            {
+                return Json(new
+                {
+                    OK = true,
+                    Mensagem = "Autenticado, redirecionando..."
+                },
+                JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new
+                {
+                    OK = false,
+                    Mensagem = "Usuário não encontrato. Tente novamente."
+                },
+                JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult Configuracoes()
 		{
 			return View();
 		}
